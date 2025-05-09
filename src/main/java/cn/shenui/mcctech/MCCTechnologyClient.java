16cfd31e4c89cb4faeca4948f20076a02e88c652
Yokey <shenui6@outlook.com>
2025-05-09 22:48:37 +0800

    Alpha 0.0.11

---
+++ src/main/java/cn/shenui/mcctech/MCCTechnologyClient.java
@@ -0,0 +1,10 @@
+package cn.shenui.mcctech;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCTechnologyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
