9817b04dd596d7a9a8151ae805efad41c8418df0
Yokey <shenui6@outlook.com>
2025-05-09 22:20:56 +0800

    1

---
+++ src/main/java/cn/shenui/mcctech/MCCTechnologyDataGenerator.java
@@ -0,0 +1,11 @@
+package cn.shenui.mcctech;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MCCTechnologyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
